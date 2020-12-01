2b5f8a9b245d676bc185366264aa1d91201d5728
James McHargue <jpmchargue@gmail.com>
2020-12-01 10:00:54 -0600

    First commit.

---
+++ api.php
@@ -0,0 +1,19 @@
+<?php
+
+header("Access-Control-Allow-Origin: ytscoop.com");
+
+if (isset($_POST['url'])) {
+  # Fetch stream attributes, stream locations, and cipher JavaScript.
+  echo "URL: " . $_POST['url'];
+
+} else if (isset($_POST['js']) && isset($_POST['choices'])) {
+  # Decipher selected stream URLs and return them.
+  # Choices should be a semicolon/comma-separated string:
+  #  - individual streams are separated by semicolons
+  #  - within each stream 'block', the stream type, quality descriptor, and ciphered URL are separated by commas
+  #    e.g. adaptive,1080p at 30fps and 192kbps,https://...
+  echo "getting cipher and handling choices";
+
+}
+
+?>
